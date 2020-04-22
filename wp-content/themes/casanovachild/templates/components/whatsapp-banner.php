a0ebc8fb8c057b6f23d870db83a4ff8ac4e6683e
Saul <saulnachman@gmail.com>
2020-04-22 22:19:27 +0200

    Change link from tel to contact page

--- wp-content/themes/casanovachild/templates/components/whatsapp-banner.php
+++ wp-content/themes/casanovachild/templates/components/whatsapp-banner.php
@@ -3,8 +3,8 @@
 <section id="get-video-quote" aria-labelledby="getVideoQuote">
   <div class="container">
     <div id="whatsapp-bar" class="flex-center">
-      <a href="tel:+27217123713" class="t-wa-green t-caps h3">Request a video quote!</a>
-      <a href="tel:+27217123713" class="flex-center wa-icon">
+      <a href="/contact-us" class="t-wa-green t-caps h3">Request a video quote!</a>
+      <a href="/contact-us" class="flex-center wa-icon">
       <?php
         require 'whatsapp.php';
       ?>
